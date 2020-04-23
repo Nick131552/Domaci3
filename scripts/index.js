eb92bd49723b23252f0cf62787bc719e544fcf38
Nick131552 <np0x04@gmail.com>
2020-04-23 20:42:14 +0200

    Commit 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 let sliderList = {elements: document.getElementById("sliderList").children, shown: [0, 1, 2]}
-
+let ratingList = {elements: document.getElementById("ratingList").children, shown: [0, 1, 2, 3, 4, 5]}
+let modalWindow = document.getElementById("modalWindow");
 
 function moveSlider(direction){
     if (direction === 'left'){
@@ -49,4 +50,41 @@ function moveSlider(direction){
             sliderList["shown"][2] = 2;
         }
     }
+}
+
+
+function openModalWindow(e){
+    let info = e.parentNode.children;
+    modalWindow.style.display = "block"
+
+    for (let i = 0; i < (info.length-1); i++){
+        modalWindow.append(info[i].cloneNode(true))
+    }
+
+}
+
+function closeModalWindow(){
+    modalWindow.style.display = "none"
+    modalWindow.innerHTML = "<button onclick=\"closeModalWindow()\">X</button>"
+}
+
+
+function loadMore(){
+    ratingList["elements"][6].style.display = "block"
+    ratingList["elements"][7].style.display = "block"
+    ratingList["elements"][8].style.display = "block"
+
+    document.getElementsByTagName("main")[0].style.height = "110vh";
+    document.getElementById("topRated").lastElementChild.innerText = "CLOSE";
+    document.getElementById("topRated").lastElementChild.onclick = close;
+}
+
+function close(){
+    ratingList["elements"][6].style.display = "none"
+    ratingList["elements"][7].style.display = "none"
+    ratingList["elements"][8].style.display = "none"
+
+    document.getElementsByTagName("main")[0].style.height = "90vh";
+    document.getElementById("topRated").lastElementChild.innerText = "LOAD MORE";
+    document.getElementById("topRated").lastElementChild.onclick = loadMore;
 }
